df5bffe819e1ba278a2ad1a62eba494b8565d442
Torin <torin@agrippan.com>
2025-02-03 13:01:36 -0500

    Testing adjustment

--- packages/testing/vite.config.ts
+++ packages/testing/vite.config.ts
@@ -1,3 +1,4 @@
+// @repo/testing/vite.config.ts
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
@@ -16,3 +17,24 @@ export const createConfig = () =>
   });
 
 export default createConfig();
+
+// import { defineConfig } from "vitest/config"
+// import react from "@vitejs/plugin-react"
+// import path from "path"
+
+// export default defineConfig({
+//   plugins: [react()],
+//   test: {
+//     environment: "jsdom",
+//     globals: true,
+//     css: true,
+//     setupFiles: ["./src/setup.ts"],
+//     include: ["../**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+//     exclude: ["**/node_modules/**", "**/dist/**"],
+//   },
+//   resolve: {
+//     alias: {
+//       "@repo/testing": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
