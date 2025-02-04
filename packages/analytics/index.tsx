dcbae2708dd8ba2f36415e33103e21aa690b2344
Torin <torin@agrippan.com>
2025-02-04 10:43:26 -0500

    Support PostHog again

--- packages/analytics/index.tsx
+++ packages/analytics/index.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from 'react';
 import { GoogleAnalytics } from './google';
 import { keys } from './keys';
+import { PostHogProvider } from './posthog/client';
 import { VercelAnalytics } from './vercel';
 
 type AnalyticsProviderProps = {
@@ -10,11 +11,11 @@ type AnalyticsProviderProps = {
 const { NEXT_PUBLIC_GA_MEASUREMENT_ID } = keys();
 
 export const AnalyticsProvider = ({ children }: AnalyticsProviderProps) => (
-  <>
+  <PostHogProvider>
     {children}
     <VercelAnalytics />
     {NEXT_PUBLIC_GA_MEASUREMENT_ID && (
       <GoogleAnalytics gaId={NEXT_PUBLIC_GA_MEASUREMENT_ID} />
     )}
-  </>
+  </PostHogProvider>
 );
