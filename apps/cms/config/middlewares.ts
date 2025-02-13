72bb60a0bfa79dc30c29b9ed01b3c82755a6e257
Torin <torin@agrippan.com>
2025-02-13 14:58:08 -0500

    Increase upload size

--- apps/cms/config/middlewares.ts
+++ apps/cms/config/middlewares.ts
@@ -5,7 +5,17 @@ export default [
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
-  'strapi::body',
+  {
+    name: 'strapi::body',
+    config: {
+      formLimit: '256mb', // modify form body limit
+      jsonLimit: '256mb', // modify JSON body limit
+      textLimit: '256mb', // modify text body limit
+      formidable: {
+        maxFileSize: 250 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+      },
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
